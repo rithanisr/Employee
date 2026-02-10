d8be2d9ebf248252c66a510e873dde514e83e730
rithanisr <rithanisr10@gmail.com>
2026-02-10 18:09:26 +0530

    deployment3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,17 @@ import Data from './components/Data';
 import Createemployee from './pages/Createemployee';
 import Login from './pages/Login';
 import Update from './pages/Update';
+import { gql, useQuery } from "@apollo/client";
+
+const HELLO_QUERY = gql`
+  query {
+    hello
+  }
+`;
 
 const App: React.FC = () => {
+    const { data, loading, error } = useQuery(HELLO_QUERY);
+
   return (
     <Container 
       className="App" 
@@ -21,6 +30,9 @@ const App: React.FC = () => {
         alignItems: "center"
       }}
     >
+       {loading && <p>Connecting to backend...</p>}
+      {error && <p style={{ color: 'red' }}>Error: {error.message}</p>}
+      {data && <p style={{ color: 'green' }}>Backend says: {data.hello}</p>}
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Header />} />
--- client/src/apollo-client.tsx
+++ client/src/apollo-client.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3003/graphql',
+  uri: 'https://employee-i0ao.onrender.com/graphql',
   
   
 }); 
