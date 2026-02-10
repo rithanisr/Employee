cc6171766058db24ace51d960eafef708a16041c
rithanisr <rithanisr10@gmail.com>
2026-02-10 16:10:49 +0530

    package changes

--- server/src/resolver/resolver.ts
+++ server/src/resolver/resolver.ts
@@ -1,5 +1,5 @@
 import User from '../models/User';
-import bcrypt from 'bcrypt';
+import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
 import { ApolloError } from 'apollo-server-express';
 
